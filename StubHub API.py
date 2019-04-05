82a329ba7cfd2d4d746c6be33c92aeebcd9b69a5
Jackson <bswxj01@railinc.com>
2019-04-05 10:37:59 -0400

    Just added a function wrapper

--- StubHub API.py
+++ StubHub API.py
@@ -8,6 +8,8 @@ Created on Fri Apr  5 09:35:23 2019
 import urllib
 import pandas as pd
 import numpy as np
+import json
+import requests
 
 
 base_url = ('https://api.seatgeek.com/2/')
@@ -16,9 +18,21 @@ client_id = ('MTM4MTIyMDZ8MTU1NDQ3MTkxMy43Ng')
 
 client_secret = ('c49766eaad2bc8bc33810d112d141ca9a09b0a78b1be52c459eb19c5fd3527a5')
 
-events_endpoint = (base_url + 'events?' + 'client_id=' + client_id + '&' + 'client_secret=' + client_secret)
 
-print(events_endpoint)
+def SeatGeek_Events():
+    
+    events_endpoint = (base_url + 'events?' + 'client_id=' + client_id + '&' + 'client_secret=' + client_secret)
+    
+    #print(events_endpoint)
+    
+    r=requests.get(events_endpoint)
+    
+    json_dat = json.loads(r.text)
+    
+    return json_dat
 
 
-urllib.request.urlopen(events_endpoint)
+
+SeatGeek_Events()
+
+print(json_dat)
