db4423a53f4c4cdee61cff56de528db3a3ec5469
wjacks4 <wjacks4@g.clemson.edu>
2019-04-06 11:38:55 -0400

    Pull detailed Event Data (Prices, Location etc)

--- Ticketmaster_API.py
+++ Ticketmaster_API.py
@@ -12,7 +12,7 @@ import pandas as pd
 import unidecode
 from unidecode import unidecode
 
-test_db = pd.read_csv("C:/Users/whjac/Downloads/data science/Ticket Flipping/Data/regional-global-daily-latest.csv")
+test_db = pd.read_csv("C:/Users/whjac/Desktop/Ticket Flipping/Event_Ticket_Pricing/Data/test.csv")
 
 #print(list(test_db.columns.values))
 
@@ -20,7 +20,7 @@ test_db = pd.read_csv("C:/Users/whjac/Downloads/data science/Ticket Flipping/Dat
 
 base_url = ('https://app.ticketmaster.com/discovery/v2/events.json?&apikey=OrCBYA46Xdvtl7RFfU88egw4L8HDPRW3&size=10&keyword=')
 
-sample=test_db.head(10)
+sample=test_db.head(1)
 
 #print(sample)
 
@@ -30,7 +30,7 @@ def EVENT_IDs (df):
 
 	artists = df['Artist']
 
-	event_df = pd.DataFrame()
+	event_ID_df = pd.DataFrame()
 	
 	for artist in artists:
 	
@@ -55,9 +55,9 @@ def EVENT_IDs (df):
 				print((id).encode('utf8'))
 				
 				
-				each_event = pd.DataFrame([[name, id]], columns=['attraction_name', 'ID'])
+				each_event_ID = pd.DataFrame([[name, id]], columns=['attraction_name', 'ID'])
 			
-				event_df = event_df.append(each_event)
+				event_ID_df = event_ID_df.append(each_event_ID)
 			
 			#print(each_event)
 				
@@ -68,9 +68,9 @@ def EVENT_IDs (df):
 			print(Oshit)
 			
 			
-	print(event_df)
+	print(event_ID_df)
 	
-	return event_df
+	return event_ID_df
 			
 
 #EVENT_IDs(sample)
@@ -91,9 +91,13 @@ def EVENT_DETAILS():
 	
 	IDs = EVENT_IDs(sample)['ID']
 	
+	event_df = pd.DataFrame()
+	
 	for event_ID in IDs:
 	
 		event_url = (event_base_url + event_ID + api_key)
+
+		print(event_url)
 		
 		raw_Data=urllib.request.urlopen(event_url)
 
@@ -101,13 +105,52 @@ def EVENT_DETAILS():
 			
 		encoded_Dat = raw_Data.read().decode('utf-8', 'ignore')			
 		json_Dat = json.loads(encoded_Dat)
-		event_venue = json_Dat['_embedded']['venues']
+		event_venue = json_Dat['_embedded']['venues'][0]['name']
+		event_city = json_Dat['_embedded']['venues'][0]['city']['name']
+		event_dates= json_Dat['dates']
+		event_sales = json_Dat['sales']
+		event_name = json_Dat['name']
+		
+		try: 
+			event_prices = json_Dat['priceRanges']
+			print(event_prices[0]['min'])
+			
+			TEST=pd.DataFrame([[event_name, event_venue, event_city, event_dates['start']['localDate'], event_sales['public']['startDateTime'], event_prices[0]['min'] ]], 
+							columns=['attraction_name', 'venue', 'city', 'event_date', 'sale_start_date', 'lowest_face_val_price'])			
+			
+		except KeyError as No_Price_Data:
+			
+			print('No Price Data Available')
+			
+			TEST=pd.DataFrame([[event_name, event_venue, event_city, event_dates['start']['localDate'], event_sales['public']['startDateTime'], '' ]], 
+						columns=['attraction_name', 'venue', 'city', 'event_date', 'sale_start_date', 'lowest_face_val_price'])			
 
-		print(event_venue)
+			
 
-		for item in event_venue:
-			city = item['city']
-			print(city)
+		#for item in event_venue:
+		#	city = item['city']['name']
+		#	venue = item['name']
+		
+			#print(city)
+			#print(venue)
+			
+		#	each_event = pd.DataFrame([[event_name, venue, city]], columns=['attraction_name', 'venue', 'city'])
+			
+		event_df = event_df.append(TEST)
+			
+		time.sleep(.25)
+			
+			
+		
+			
+		
+			
+		
+	event_df.to_csv('C:/Users/whjac/Desktop/Ticket Flipping/Event_Ticket_Pricing/Data/Ticketmaster_event_list.csv', index=False)
+	
+	print(event_df)
+	
+	return event_df
 			
 			
 EVENT_DETAILS()
