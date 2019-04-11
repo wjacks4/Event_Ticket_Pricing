1cd2b3e425e31606b8ded14bf1b667a892cc0e10
Jackson <bswxj01@railinc.com>
2019-04-11 15:02:09 -0400

    First barebones of Eventbrite API pull

---
+++ EventBrite_API.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 15:00:58 2019
+
+@author: bswxj01
+"""
+
+import mysql
+from mysql.connector import Error
+import psycopg2 as p
+import json
+from dateutil import parser
+import time
+import os
+import subprocess
+import urllib
+import urllib.request
+import pandas as pd
+import unidecode
+from unidecode import unidecode
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.client import Spotify
+import requests
+import urllib
+from urllib import parse
