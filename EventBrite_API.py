16f6a52c18b4818975255dfa62e585e632dc230f
wjacks4 <wjacks4@g.clemson.edu>
2019-04-25 00:03:12 -0400

    justin case

--- EventBrite_API.py
+++ EventBrite_API.py
@@ -53,11 +53,14 @@ def EventBrite_Artist_Search(df):
 	
 	artists = sample['Artist']
 	
+	
+	
 	for artist in artists:
 		
 		artist_encode = artist.replace(" ", "%20")
-		artist_url = (base_string + "q=" + artist_encode)
+		artist_url = (base_string + "expand=ticket_availability,external_ticketing,venue&" + "q=" + artist_encode)
 		
+		print(artist_url)
 		rawdat = urllib.request.urlopen(artist_url)
 		encoded_dat = rawdat.read().decode('utf-8', errors='ignore')
 		
@@ -66,24 +69,46 @@ def EventBrite_Artist_Search(df):
 		json_dat = json.loads(encoded_dat)
 		events = json_dat['events']
 		
+		event_df = pd.DataFrame()
+			
+		attr_list = ['name', 'id', 'start', 'end', 'capacity', 'listed', 'shareable', 'venue_id']
+			
 		for event in events:
 		
-			name = event['name']
-			id = event['id']
-			start = event['start']
-			end = event['end']
-			capacity = event['capacity']
-			listed = event['listed']
-			shareable = event['shareable']
-			venue_id = event['venue_id']
+			try:
+				name = event['name']
+				id = event['id']
+				start = event['start']
+				end = event['end']
+				capacity = event['capacity']
+				listed = event['listed']
+				shareable = event['shareable']
+				venue_id = event['venue_id']
+				
+				venue_state = event['venue']['address']['region']
+				venue_city = event['venue']['address']['city']
+				
+				minimum_price = event['ticket_availability']['minimum_ticket_price']['major_value']
+				maximum_price = event['ticket_availability']['maximum_ticket_price']['major_value']
+				sold_out_indicator = event['ticket_availability']['is_sold_out']
+				start_date_utc = event['ticket_availability']['start_sales_date']
+				available_elsewhere = event['is_externally_ticketed']
 			
-			print(name)
-			for obj in event:
-				print(obj)
-	
+				event_profile=pd.DataFrame([[name, id, start, end, capacity, listed, shareable, venue_id, venue_state, venue_city, minimum_price, maximum_price, sold_out_indicator, start_date_utc, available_elsewhere]], 
+					columns=['event_name', 'event_id', 'event_start', 'event_end', 'event_capacity', 'listed', 'shareable', 'venue_id', 'venue_state', 'venue_city', 'minimum_price', 'maximum_price', 'sold_out_indicator', 'start_date_utc', 'available_elsewhere'])	
+				
+				event_df = event_df.append(event_profile)
+					
+			except TypeError as no_data:
+			
+				print ('One of the fields was missing')
+			
+			
 
 		
-		#print(json_dat)
+			
+		
+		print(event_df)
 		
 		
 
