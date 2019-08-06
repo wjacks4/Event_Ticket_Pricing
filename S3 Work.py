99d6cd8fa18733faff3a59b8d2bca101f2f3c43b
Will <wjacks4@g.clemson.edu>
2019-08-05 22:22:25 -0400

    Successfully pulled back pickle from s3

--- S3 Work.py
+++ S3 Work.py
@@ -20,6 +20,7 @@ from urllib import parse
 import sys
 import base64
 import numpy as np
+import re
 import pymysql
 # import MySQLdb
 import base64
@@ -96,4 +97,53 @@ def test_s3():
     key='test.pkl'
     s3_resource.Object(bucket,key).put(Body=event_json)
 
-test_s3()
+#test_s3()
+
+
+
+"""
+TEST PULLING DATA FROM EXISTING MYSQL INTO PANDAS, TURNING INTO JSON, THEN PICKLING IN S3
+"""
+
+def mysql_to_s3():
+
+    connection = connection = pymysql.connect(host='ticketsdb.cxrz9l1i58ux.us-west-2.rds.amazonaws.com',
+                                              user='tickets_user', password='tickets_pass', db='tickets_db')
+
+    eventbrite_df = pd.read_sql('SELECT * FROM EVENTBRITE_EVENTS LIMIT 1000', con=connection)
+
+    print((eventbrite_df).head(20))
+
+    eventbrite_json = eventbrite_df.to_json(orient='records')
+
+    s3_resource = boto3.resource('s3')
+
+    bucket='willjeventdata'
+    key='eventbrite_test.pkl'
+    s3_resource.Object(bucket,key).put(Body=eventbrite_json)
+
+# mysql_to_s3()
+
+"""
+TEST PULLING BACK PICKLED DATA
+"""
+
+def pickle_pull():
+
+    s3_resource = boto3.client('s3')
+    bucket = 'willjeventdata'
+    key = 'SeatGeek_Events.pkl'
+    response = s3_resource.get_object(Bucket=bucket, Key=key)
+    test=(response['Body'].read())
+    print(test.decode('utf8'))
+
+    json_obj = json.loads(test.decode('utf8'))
+
+pickle_pull()
+
+
+
+
+
+
+
